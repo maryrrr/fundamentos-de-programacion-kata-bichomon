bd2b2a6c951aeb77f6d618040aada8dca3565801
maryrrr <mary.russu83@gmail.com>
2023-03-06 19:52:00 +0100

    agregar inform

--- scripts.js
+++ scripts.js
@@ -1,1 +1,23 @@
 console.log(document.title);
+const titulo=document.getElementById('gen-1')
+titulo.innerHTML="Generasión 1 Pokimon"
+console.log(titulo)
+
+const cambFondo=document.querySelector('.infocard')
+cambFondo.style.backgroundColor="blue"
+
+const impUrl=document.location.href
+console.log(impUrl)
+
+const impDominio=document.location.host
+console.log(impDominio)
+
+const nodosImage=document.querySelectorAll('.infocard-lg-img')
+console.log(nodosImage)
+
+const img = document.querySelectorAll('img')
+.forEach(img => img.src = "https://media.giphy.com/media/2v170e71aanfi/giphy.gif");
+console.log(img)
+
+
+
